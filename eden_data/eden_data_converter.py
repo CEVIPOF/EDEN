05c1e9e55392d2cc5d6e33e91043cd6e27772f68
diegantobass <diego.antolinosbasso@sciencespo.fr>
2021-03-30 09:56:51 +0200

    add parsing of .VIL files

--- eden_data/eden_data_converter.py
+++ eden_data/eden_data_converter.py
@@ -52,10 +52,18 @@ for election in os.listdir(folder_input):
             src_file = open(src_file_path, 'r', encoding='CP437')
             print("\tParsing .CIR file - (path : " + src_file_path + ")")
 
+            # set output path, open output file stream, write output headers
+            out_path = src_file_path.replace('src', 'csv') + ".csv"
+            out_file = csv.writer(open(out_path, 'w'))
+            out_headers = headers + parsenomnom(src_file_path.replace('.CIR', '.NOM'))
+            out_file.writerow(out_headers)
+
             for line in src_file:
                 # get infos from fixed-width .CIR csv
                 dep_num = line[:2]
                 circo_num = line[2:4]
+                # TODO : check how to build circonum
+                circo_num = (dep_num + circo_num).strip().zfill(4)
                 elec_type = line[4]
                 elec_year = line[5:7]
                 elec_turn = line[7]
@@ -65,14 +73,20 @@ for election in os.listdir(folder_input):
                 results = [results[i:i + 10].strip() for i in range(0, len(results)-1, 10)]
                 results = [x.split()[1] for x in results]
                 # print(results)
-            print("\tDone .CIR file\n")
 
+                # create output csv line from the infos extracted, write it to output file stream
+                out_line = [circo_num, elec_types[elec_type], whatyearisthis(elec_year), elec_turn, inscrits, votants, exprimes]
+                # print(out_line)
+                out_line.extend(results)
+                out_file.writerow(out_line)
+
+        # if the file is a .DEP file, extract its content
         elif src_file.endswith('.DEP'):
             src_file = open(src_file_path, 'r', encoding='CP437')
             print("\tParsing .DEP file - (path : " + src_file_path + ")")
 
             # set output path, open output file stream, write output headers
-            out_path = src_file_path.replace('src', 'csv').replace('.DEP', '.csv')
+            out_path = src_file_path.replace('src', 'csv') + ".csv"
             out_file = csv.writer(open(out_path, 'w'))
             out_headers = headers + parsenomnom(src_file_path.replace('.DEP', '.NOM'))
             out_file.writerow(out_headers)
@@ -80,7 +94,7 @@ for election in os.listdir(folder_input):
             for line in src_file:
                 # print(line)
                 # get infos from fixed-width .DEP csv
-                dep_num = line[:2]
+                dep_num = (line[:2]).strip().zfill(2)
                 elec_type = line[2]
                 elec_year = line[3:5]
                 elec_turn = line[5]
@@ -95,7 +109,37 @@ for election in os.listdir(folder_input):
                 # print(out_line)
                 out_line.extend(results)
                 out_file.writerow(out_line)
-            print("\tDone .DEP file\n")
-
 
         # if the file is a .VIL file, extract its content
+        elif src_file.endswith('.VIL'):
+            src_file = open(src_file_path, 'r', encoding='CP437')
+            print("\tParsing .VIL file - (path : " + src_file_path + ")")
+
+            # set output path, open output file stream, write output headers
+            out_path = src_file_path.replace('src', 'csv') + ".csv"
+            out_file = csv.writer(open(out_path, 'w'))
+            out_headers = headers + parsenomnom(src_file_path.replace('.VIL', '.NOM'))
+            out_file.writerow(out_headers)
+
+            for line in src_file:
+                # print(line)
+                # get infos from fixed-width .DEP csv
+                dep_num = line[:2]
+                vil_num = line[2:5]
+                # TODO : check how to build vilnum
+                vil_num = (dep_num + vil_num).strip().zfill(5)
+                elec_type = line[5]
+                elec_year = line[6:8]
+                elec_turn = line[8]
+                elec_info = line[9:33]
+                [inscrits, votants, exprimes] = [int(elec_info[i:i + 8].strip()) for i in range(0, len(elec_info), 8)]
+                results = line[33:]
+                results = [results[i:i + 10].strip() for i in range(0, len(results)-1, 10)]
+                results = [x.split()[1] for x in results]
+
+                # create output csv line from the infos extracted, write it to output file stream
+                out_line = [vil_num, elec_types[elec_type], whatyearisthis(elec_year), elec_turn, inscrits, votants, exprimes]
+                # print(out_line)
+                out_line.extend(results)
+                out_file.writerow(out_line)
+    print("\n")
